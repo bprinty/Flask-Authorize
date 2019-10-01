dc23123d7ea60f87df511e8d83a2a43b324dc49b
bprinty <bprinty@gmail.com>
2019-09-30 20:56:23 -0500

    Added test requirements, initialization

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+TESTS = os.path.dirname(os.path.realpath(__file__))
+BASE = os.path.realpath(os.path.join(TESTS, '..'))
+RESOURCES = os.path.join(TESTS, 'resources')
+SANDBOX = os.path.join(TESTS, 'sandbox')
+
