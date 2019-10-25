b91c74ecfa77322f736cd8fdf415b10a86d77834
bprinty <bprinty@gmail.com>
2019-10-25 00:18:55 -0500

    fixed side-icond path

--- docs/conf.py
+++ docs/conf.py
@@ -141,8 +141,8 @@ html_theme_path = []
 
 # The name of an image file (relative to this directory) to place at the
 # top of the sidebar.
-html_logo = "side-icon.png"
-html_favicon = "side-icon.png"
+html_logo = "_static/side-icon.png"
+html_favicon = "_static/side-icon.png"
 
 # The name of an image file (within the static path) to use as favicon
 # of the docs.  This file should be a Windows icon file (.ico) being
