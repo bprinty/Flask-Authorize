94a2edd49d87b45d00d6dbdd4ddc53afa7bc916c
bprinty <bprinty@gmail.com>
2019-10-25 22:57:06 -0500

    Added first pass at database mixins for module

--- flask_authorize/mixins.py
+++ flask_authorize/mixins.py
@@ -7,8 +7,9 @@
 
 # imports
 # -------
-from flask import current_app
+import re
 import json
+from flask import current_app
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy.types import Integer, String
 from sqlalchemy.orm import relationship, backref
@@ -46,6 +47,42 @@ class Permission(TypeDecorator):
 
 # helpers
 # -------
+MODELS = dict()
+
+
+def gather_models():
+    """
+    Inspect sqlalchemy models from current context and set global
+    dictionary to be used in url conversion.
+    """
+    global MODELS
+
+    from flask import current_app
+    if 'sqlalchemy' not in current_app.extensions:
+        return
+
+    # inspect current models and add to map
+    db = current_app.extensions['sqlalchemy'].db
+    for cls in db.Model._decl_class_registry.values():
+        if isinstance(cls, type) and issubclass(cls, db.Model):
+
+            # class name
+            if current_app.config['AUTHORIZE_MODEL_PARSER'] == 'default':
+                MODELS[cls.__name__] = cls
+
+            # lowercase name
+            elif current_app.config['AUTHORIZE_MODEL_PARSER'] == 'lower':
+                MODELS[cls.__name__.lower()] = cls
+
+            # snake_case name
+            elif current_app.config['AUTHORIZE_MODEL_PARSER'] == 'snake':
+                words = re.findall(r'([A-Z][0-9a-z]+)', cls.__name__)
+                if len(words) > 1:
+                    alias = '_'.join(map(lambda x: x.lower(), words))
+                    MODELS[alias] = cls
+    return
+
+
 def default_permissions(cls):
     """
     Return default permissions for model, falling
@@ -55,41 +92,86 @@ def default_permissions(cls):
     if cls.__permissions__ is None:
         return current_app.config['AUTHORIZE_DEFAULT_PERMISSIONS']
     elif isinstance(cls._permissions__, int):
-        return parse_numeric_permissions(cls.__permissions__)
+        return parse_permission_set(cls.__permissions__)
     elif isinstance(cls.__permissions__, dict):
         return cls.__permissions__
 
 
-# def number_to_permission_list():
-#     for mask, name in zip([1, 2, 4], ['delete', 'read', 'delete']):
-#         perm = int(number) // 10 ** digit % 10
-#         if perm & mask:
-#             STOPPED HERE
-#     return
+def default_allowances(cls):
+    """
+    Return default permissions for model, falling
+    back to app configuration if no default permission
+    is explicitly set.
+    """
+    if not isinstance(cls.__allowances__, dict):
+        raise AssertionError('Allowances for model {} must be dictionary type!'.format(cls.__name__))
 
-def parse_numeric_permissions(number):
+    # if necessary, gather database models to create default
+    global MODELS
+    if not MODELS:
+        gather_models()
+    default = {
+        key: current_app.config['AUTHORIZE_DEFAULT_ALLOWANCES']
+        for key in MODELS
+    }
+
+    # overwrite specified allowances
+    return default.update(cls.__allowances__)
+
+
+def default_restrictions(cls):
+    """
+    Return default permissions for model, falling
+    back to app configuration if no default permission
+    is explicitly set.
+    """
+    if not isinstance(cls.__restrictions__, dict):
+        raise AssertionError('Restrictions for model {} must be dictionary type!'.format(cls.__name__))
+
+    # if necessary, gather database models to create default
+    global MODELS
+    if not MODELS:
+        gather_models()
+    default = {
+        key: current_app.config['AUTHORIZE_DEFAULT_RESTRICTIONS']
+        for key in MODELS
+    }
+
+    # overwrite specified allowances
+    return default.update(cls.__restrictions__)
+
+
+def permission_list(number):
+    """
+    Generate permission list from numeric input.
+    """
+    ret = []
+    for mask, name in zip([1, 2, 4], ['delete', 'read', 'update']):
+        if number & mask:
+            ret.append(name)
+    return ret
+
+
+def parse_permission_set(number):
     """
     Parse numeric permissions and return dictionary with
     explicit permission scheme. Note that this method
     does not account for custom content permissions.
     """
+    if not isinstance(number, int):
+        return number
+
     # check validity of input
     digits = len(str(number))
     if digits > 3:
         raise AssertionError('Invalid permissions: {}'.format(number))
 
     # gather permissions
-    permissions = dict(
-        owner=[],
-        group=[],
-        other=[]
-    )
+    result = {}
     for digit, check in zip([0, 1, 2], ['other', 'group', 'owner']):
-        for mask, name in zip([1, 2, 4], ['delete', 'read', 'delete']):
-            perm = int(number) // 10 ** digit % 10
-            if perm & mask:
-                permissions[check].append(name)
-    return permissions
+        perm = int(number) // 10 ** digit % 10
+        result[check] = permission_list(perm)
+    return result
 
 
 # permissions mixins
@@ -100,7 +182,7 @@ class BasePermissionsMixin(object):
     across various optional permission schemes.
     """
     __permissions__ = None
-    
+
     # properties
     @declared_attr
     def permissions(cls):
@@ -112,7 +194,7 @@ class BasePermissionsMixin(object):
         """
         # handle numeric permission scheme
         if len(args):
-            perms = parse_numeric_permissions(args[0])
+            perms = parse_permission_set(args[0])
             permissions.update(perms)
 
         # set internal permissions object
@@ -120,7 +202,7 @@ class BasePermissionsMixin(object):
         return
 
 
-class OwnerMixin(BasePermissionsMixin):
+class OwnerMixin(object):
     """
     Mixin providing owner-related database properties
     for object, in the context of enforcing permissions.
@@ -138,7 +220,11 @@ class OwnerMixin(BasePermissionsMixin):
         ))
 
 
-class GroupMixin(BasePermissionsMixin):
+class OwnerPermissionsMixin(BasePermissionsMixin, OwnerMixin):
+    pass
+
+
+class GroupMixin(object):
     """
     Mixin providing group-related database properties
     for object, in the context of enforcing permissions.
@@ -156,12 +242,18 @@ class GroupMixin(BasePermissionsMixin):
         ))
 
 
-class MultiGroupMixin(BasePermissionsMixin):
+class GroupPermissionsMixin(BasePermissionsMixin, GroupMixin):
+    pass
+
+
+class MultiGroupMixin(object):
     """
     Mixin providing groups-related database properties
     for object, in the context of enforcing permissions.
 
     .. note:: NEEDS MORE DOCUMENTATION AND EXAMPLES
+
+    .. note:: NEED TO FIGURE OUT HOW TO AUTOMATICALLY CREATE MAPPING TABLE
     """
     @declared_attr
     def groups(cls):
@@ -170,7 +262,11 @@ class MultiGroupMixin(BasePermissionsMixin):
         ))
 
 
-class PermissionsMixin(OwnerMixin, GroupMixin):
+class MultiGroupPermissionsMixin(BasePermissionsMixin, MultiGroupMixin):
+    pass
+
+
+class PermissionsMixin(BasePermissionsMixin, OwnerMixin, GroupMixin):
     """
     Mixin providing owner and group-related database properties
     for object, in the context of enforcing permissions.
@@ -180,30 +276,29 @@ class PermissionsMixin(OwnerMixin, GroupMixin):
     pass
 
 
-# restrictions mixins
-# -------------------
-class BaseRestrictionsMixin(object):
+# rbac mixins
+# -----------
+class RestrictionsMixin(object):
     """
-    Abstract base class for enabling common functionality
-    across various optional permission schemes.
+    Mixin providing group or role based access control.
+
+    .. note:: NEEDS MORE DOCUMENTATION AND EXAMPLES
     """
-    __restrictions__ = None
-    
-    # properties
+    __restrictions__ = dict()
+
     @declared_attr
     def restrictions(cls):
-        return Column(Permission, default=cls.__permissions__)
+        return Column(Permission, default=default_restrictions)
 
-    def set_restrictions(self, *args, **permissions):
-        """
-        Set permissions explicitly for ACL-enforced content.
-        """
-        # handle numeric permission scheme
-        if len(args):
-            for key
-            perms = parse_numeric_permissions(args[0])
-            permissions.update(perms)
 
-        # set internal permissions object
-        self.permissions.update(permissions)
-        return
+class AllowancesMixin(object):
+    """
+    Mixin providing group or role based access control.
+
+    .. note:: NEEDS MORE DOCUMENTATION AND EXAMPLES
+    """
+    __allowances__ = dict()
+
+    @declared_attr
+    def allowances(cls):
+        return Column(Permission, default=default_allowances)
