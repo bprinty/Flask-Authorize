30ad1f1afa436efc00506d17f68ced824e2db365
bprinty <bprinty@gmail.com>
2019-10-29 00:10:13 -0500

    Added tests for role and group based acess control

--- tests/test_access.py
+++ tests/test_access.py
@@ -7,33 +7,55 @@
 
 # imports
 # -------
-from .fixtures import ArticleFactory
+from flask import g
+
+from .fixtures import ArticleFactory, authorize
 
 
 # tests
 # -----
 class TestAllowances(object):
 
-    def test_role_allowances(self, client, users):
-        return
-
-    def test_group_allowances(self, client, users):
+    def test_allowances(self, client, allowed, unallowed):
         return
 
 
 class TestRestrictions(object):
 
-    def test_role_restrictions(self, client, users):
-        return
-
-    def test_group_restrictions(self, client, users):
+    def test_restrictions(self, client, restricted, unrestricted):
         return
 
 
 class TestCredentials(object):
 
-    def test_in_group(self, client, users):
+    def test_in_group(self, client, admin, reader, editor):
+        article = ArticleFactory.create(
+            name='Closed Article',
+            owner=editor,
+            group=editor.groups[0]
+        ).set_permissions('000')
+
+        g.user = reader
+        assert authorize.in_group('readers')(article)
+        assert not authorize.in_group('editors')(article)
+
+        g.user = editor
+        assert authorize.in_group('editors')(article)
+        assert not authorize.in_group('readers')(article)
         return
 
-    def test_has_role(self, client, users):
+    def test_has_role(self, client, reader, editor):
+        article = ArticleFactory.create(
+            name='Closed Article',
+            owner=editor,
+            group=editor.groups[0]
+        ).set_permissions('000')
+
+        g.user = reader
+        assert authorize.has_role('readers')(article)
+        assert not authorize.has_role('editors')(article)
+
+        g.user = editor
+        assert authorize.in_group('editors')(article)
+        assert not authorize.in_group('readers')(article)
         return
