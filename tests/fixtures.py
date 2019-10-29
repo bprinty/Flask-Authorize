74909776658d73181780c825f7f3d980a1a406d0
bprinty <bprinty@gmail.com>
2019-10-28 22:37:24 -0500

    working out fixtures for various types of tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -12,7 +12,8 @@ import factory
 from flask import Flask, request, jsonify, g
 from werkzeug.exceptions import Unauthorized, NotFound
 from flask_sqlalchemy import SQLAlchemy
-from flask_authorize import Authorize, PermissionsMixin
+from flask_authorize import Authorize, PermissionsMixin, AllowancesMixin, RestrictionsMixin
+from flask_authorize.mixins import default_allowances, default_restrictions
 
 from . import SANDBOX
 
@@ -31,71 +32,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 authorize = Authorize(app, current_user=lambda: g.user)
-
-
-@app.route('/articles', methods=['GET', 'POST'])
-def articles():
-    if request.method == 'GET':
-        if not authorize.read(Article):
-            return Unauthorized
-        articles = db.session.query(Article).all()
-        return jsonify([dict(id=a.id, name=a.name) for a in articles]), 200
-
-    elif request.method == 'POST':
-        if not authorize.write(Article):
-            return Unauthorized
-        article = Article(**request.json)
-        db.session.add(article)
-        db.session.commit()
-        return jsonify(id=article.id, name=article.name), 200
-
-    return
-
-
-@app.route('/articles/<int:ident>', methods=['GET', 'PUT', 'DELETE'])
-def single_article(ident):
-    article = db.session.query(Article).filter_by(id=ident).first()
-    if not article:
-        raise NotFound
-    if request.method == 'GET':
-        if not authorize.read(article):
-            raise Unauthorized
-        return jsonify(id=article.id, name=article.name), 200
-
-    elif request.method == 'PUT':
-        if not authorize.write(article):
-            raise Unauthorized
-        for key, value in request.json.items():
-            setattr(article, key, value)
-        db.session.commit()
-        return jsonify(id=article.id, name=article.name), 200
-
-    elif request.method == 'DELETE':
-        if not authorize.has_role('admin'):
-            raise Unauthorized
-        db.session.delete(article)
-        db.session.commit()
-
-    return
-
-
-@authorize.read
-def get_article(article):
-    return dict(
-        id=article.id,
-        name=article.name
-    )
-
-
-@authorize.write
-def update_article(article, **kwargs):
-    for k, v in kwargs.items():
-        setattr(article, k, v)
-    db.session.commit()
-    return dict(
-        id=article.id,
-        name=article.name
-    )
 
 
 # models
@@ -117,46 +53,33 @@ UserRole = db.Table(
 class User(db.Model):
     __tablename__ = 'users'
 
-    # basic
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True, index=True)
-
-    # relationships
     roles = db.relationship('Role', secondary=UserRole)
     groups = db.relationship('Group', secondary=UserGroup)
 
 
-class Group(db.Model):
+class Group(db.Model, RestrictionsMixin):
     __tablename__ = 'groups'
 
-    # basic
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True, index=True)
     desc = db.Column(db.String(255))
-    # restrictions = db.Column(db.JSON, nullable=False)
-
-    # relationships
     users = db.relationship('User', secondary=UserGroup)
 
 
-class Role(db.Model):
+class Role(db.Model, AllowancesMixin):
     __tablename__ = 'roles'
 
-    # basic
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True, index=True)
     desc = db.Column(db.String(255))
-    # restrictions = db.Column(db.JSON, nullable=False)
-
-    # relationships
     users = db.relationship('User', secondary=UserRole)
 
 
 class Article(db.Model, PermissionsMixin):
     __tablename__ = 'articles'
-    __permissions__ = '666'
 
-    # basic
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), index=True, nullable=False)
 
@@ -237,3 +160,47 @@ def users(client):
     yield users
 
     return
+
+
+def allowed(client):
+    role = RoleFactory.create(
+        name='allowed',
+        allowances=default_allowances()
+    )
+    yield UserFactory.create(
+        name='allowed',
+        roles=[role]
+    )
+
+
+def unallowed(client):
+    role = RoleFactory.create(
+        name='unallowed',
+        allowances={}
+    )
+    yield UserFactory.create(
+        name='unallowed',
+        roles=[role]
+    )
+
+
+def restricted(client):
+    group = GroupFactory.create(
+        name='restricted',
+        restrictions={'articles': default_allowances()}
+    )
+    yield UserFactory.create(
+        name='restricted',
+        roles=[group]
+    )
+
+
+def unrestricted(client):
+    group = GroupFactory.create(
+        name='unrestricted',
+        restrictions={'articles': default_restrictions()}
+    )
+    yield UserFactory.create(
+        name='unrestricted',
+        roles=[group]
+    )
