67520950beb2f24af9729e8c5c5fd3500fb52368
bprinty <bprinty@gmail.com>
2019-10-31 00:18:28 -0500

    Better description

--- flask_authorize/__init__.py
+++ flask_authorize/__init__.py
@@ -2,7 +2,7 @@
 
 __pkg__ = 'Flask-Authorize'
 __url__ = 'https://github.com/bprinty/Flask-Authorize'
-__info__ = 'Flask plugin for managing content access authorization'
+__info__ = 'Flask plugin for content authorization and access control'
 __author__ = 'Blake Printy'
 __email__ = 'bprinty@gmail.com'
 __version__ = '0.1.0'
