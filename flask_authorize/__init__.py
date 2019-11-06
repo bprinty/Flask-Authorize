a9f860b31392392d75768b5d1d8216a987091cbf
bprinty <bprinty@gmail.com>
2019-11-05 23:33:08 -0600

    Updated patch version for minor bug fix

--- flask_authorize/__init__.py
+++ flask_authorize/__init__.py
@@ -5,7 +5,7 @@ __url__ = 'https://github.com/bprinty/Flask-Authorize'
 __info__ = 'Flask plugin for content authorization and access control'
 __author__ = 'Blake Printy'
 __email__ = 'bprinty@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 
 from .mixins import RestrictionsMixin           ## noqa
